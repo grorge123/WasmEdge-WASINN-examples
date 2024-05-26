c55438db37047fd222d1fce8f014991e7f8d3655
grorge <g97116@yahoo.com>
2024-05-26 20:05:17 +0800

    use local tokenize

--- wasmedge-neuralspeed/benchmark/neural.py
+++ wasmedge-neuralspeed/benchmark/neural.py
@@ -1,23 +1,21 @@
-from transformers import AutoTokenizer, TextStreamer
-#from intel_extension_for_transformers.transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer, TextStreamer, AutoConfig
 from datetime import datetime
 from neural_speed import Model
-# Specify the GGUF repo on the Hugginface
-model_name = "TheBloke/Llama-2-7B-Chat-GGUF"
-# Download the the specific gguf model file from the above repo
-model_file = "llama-2-7b-chat.Q4_0.gguf"
-# make sure you are granted to access this model on the Huggingface.
-tokenizer_name = "meta-llama/Llama-2-7b-chat-hf"
+import sys
+if __name__ == "__main__":
+	model_name = "TheBloke/Llama-2-7B-Chat-GGUF"
+	model_file = "llama-2-7b-chat.Q4_0.gguf"
+	tokenizer_path = "/"
 
-prompt = "Once upon a time, there existed a little girl,"
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
-inputs = tokenizer(prompt, return_tensors="pt").input_ids
-streamer = TextStreamer(tokenizer)
-# model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
-model = Model()
-model.init_from_bin("llama", "/home/george/LFX-mentorship/tokenizersRust/llama-2-7b-chat.Q4_0.gguf")
-start=datetime.now()
-outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
-end=datetime.now()
-print("runTime: ", (end - start).total_seconds())
-print(len(outputs[0]))
+	prompt = "Once upon a time, there existed a little girl,"
+	tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, config=AutoConfig.from_pretrained(model_name))
+	inputs = tokenizer(prompt, return_tensors="pt").input_ids
+	streamer = TextStreamer(tokenizer)
+	# model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
+	model = Model()
+	model.init_from_bin("llama", model_file)
+	start=datetime.now()
+	outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
+	end=datetime.now()
+	print("runTime: ", (end - start).total_seconds())
+	print(len(outputs[0]))
