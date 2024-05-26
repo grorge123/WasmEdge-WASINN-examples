74905f8c610636fb5cb17ef686212a0ce46c5129
grorge <g97116@yahoo.com>
2024-05-26 14:16:54 +0800

    fix: enable log

--- wasmedge-neuralspeed/benchmark/run.sh
+++ wasmedge-neuralspeed/benchmark/run.sh
@@ -9,7 +9,7 @@ for i in $(seq 1 $TEST_TIMES); do
 	export WASMEDGE_PLUGIN_PATH=/.wasmedge-neuralspeed/lib/wasmedge
 	/.wasmedge-neuralspeed/bin/wasmedge --dir .:. --nn-preload default:NeuralSpeed:AUTO:llama-2-7b-chat.Q4_0.gguf /wasmedge-neural-speed.wasm default > /output/neuralspeed-wasi-$timestamp.log 2>&1
 	export WASMEDGE_PLUGIN_PATH=/.wasmedge-ggml/lib/wasmedge
-	/.wasmedge-ggml/bin/wasmedge --dir .:. --env enable-log=1 --nn-preload default:GGML:AUTO:llama-2-7b-chat.Q4_0.gguf /wasmedge-ggml-llama.wasm default "Once upon a time, there existed a little girl," > /output/ggml-wasi-$timestamp.log 2>&1
+	/.wasmedge-ggml/bin/wasmedge --dir .:. --env enable_log=true --nn-preload default:GGML:AUTO:llama-2-7b-chat.Q4_0.gguf /wasmedge-ggml-llama.wasm default "Once upon a time, there existed a little girl," > /output/ggml-wasi-$timestamp.log 2>&1
 	python3 /neural.py > /output/neural-py-$timestamp.log 2>&1
 	
 
